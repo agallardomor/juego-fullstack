729d99bd4d814fe11868f3af0b7c26ae6ae1f47a
Alex Gallardo <alexgallardo11@gmail.com>
2022-11-10 19:03:50 +0100

    avatar btn

--- register.js
+++ register.js
@@ -17,11 +17,16 @@ let dataRead = function(){
     console.log(
         document.querySelector("#typeEmailX").value,
         document.querySelector("#typePasswordX").value,
+      
     );
 
     let myObjectResgistration ={
         name: document.querySelector("#typeEmailX").value,
         email: document.querySelector("#typePasswordX").value,
+        avatar1: document.querySelector("#avatar1").value,
+        avatar2: document.querySelector("#avatar2").value,
+        avatar3: document.querySelector("#avatar3").value,
+       
     };
     console.log(myObjectResgistration);
 
@@ -39,7 +44,13 @@ let readLocalStorage = function(){
     let myObject = JSON.parse(miRegistro);
     document.querySelector("#typeEmailX").value = myObject.email;
     document.querySelector("#typePasswordX").value = myObject.password;
+    
     console.log(myObject)
 }
+
+let registrarse = document.querySelector("#registrarse")
+
+
+
 
 main();
